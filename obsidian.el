f1723e7a1edd00f814979cce52d062e32c73b536
MB <mb@blaster.ai>
2022-07-27 20:49:49 +0100

    Update version

--- obsidian.el
+++ obsidian.el
@@ -5,7 +5,7 @@
 ;; Author: Mykhaylo Bilyanskyy
 ;; URL: https://github.com./licht1stein/obsidian.el
 ;; Keywords: obsidian, pkm, convenience
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((emacs "27.2") (company "0.9.13") (s "1.12.0") (dash "2.13") (org "9.5.3") (markdown-mode "2.6"))
 
 ;; This file is NOT part of GNU Emacs.
