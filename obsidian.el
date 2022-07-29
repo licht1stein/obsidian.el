42392840398e80741cbe5bcb13a7b0582bcf6435
MB <mb@blaster.ai>
2022-07-29 23:29:22 +0100

    Add defgroup

--- obsidian.el
+++ obsidian.el
@@ -51,6 +51,10 @@
 (obsidian-comment
  (package-buffer-info))
 
+(defgroup obsidian nil
+  "Work with Obsidian Notes in Emacs."
+  :prefix "obsidian-")
+
 (defcustom obsidian-directory nil
   "Path to Obsidian Notes vault."
   :group 'obsidian
