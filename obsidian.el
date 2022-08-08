49b4712b407a3542bbba50d3f74e1bb45025e255
MB <mb@blaster.ai>
2022-08-09 00:51:16 +0100

    Better comment

--- obsidian.el
+++ obsidian.el
@@ -28,10 +28,12 @@
 
 
 ;;; Commentary:
-;;
-;; obsidian.el lets you interact with more convenience with markdown files
+;; Obsidian.el lets you interact with more convenience with markdown files
 ;; that are contained in Obsidian Notes vault.  It adds autocompletion for
 ;; tags and links, jumping between notes, capturing new notes into inbox etc.
+;;
+;; This allows you to use Emacs for editing your notes, leaving the Obsidian
+;; app for syncing and doing more specialized stuff, like viewing notes graphs.
 
 ;;; Code:
 (require 'dash)
