030e282040291398a1c8b28ee364e459e05aba7e
MB <mb@blaster.ai>
2022-09-10 21:12:48 +0100

    Update linting prefs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,2 @@
-((emacs-lisp-mode . ((fill-column . 100)
-                     (indent-tabs-mode . nil)
-                     (elisp-lint-ignored-validators . ("byte-compile" "package-lint" "fill-column"))
-                     (elisp-lint-indent-specs . ((describe . 2)
-                                                 (it . 2))))))
+((emacs-lisp-mode . ((indent-tabs-mode . nil)
+                     (elisp-lint-indent-specs . ((defhydra . 2))))))
