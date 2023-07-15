04216388b0078a083fb26c9e30b891a7b2588d6d
Joe Reinhart <joseph.reinhart@gmail.com>
2023-07-15 05:25:25 -0500

    Jayemar/create links with filenames only (#52)
    * Return filename only without path from obsidian--request-link
    
    * Remove unintentional newline
    
    * Add defcustom to specifiy if paths should be used for links, and allow toggling this with a prefix argument
    
    * Update the README to reflect usage of the obsidian-links-use-vault-path variable as well as the command prefix
    
    * Use more idiomatic form for interactive instead of current-prefix-arg variable
    
    * Create new helper function to determine link format

--- obsidian.el
+++ obsidian.el
@@ -67,6 +67,10 @@
   "Subdir to create notes using `obsidian-capture'."
   :type 'directory)
 
+(defcustom obsidian-links-use-vault-path nil
+  "If true, the full vault path for a link will be used instead of just the filename."
+  :type 'boolean)
+
 (eval-when-compile (defvar local-minor-modes))
 
 ;;;###autoload
@@ -354,21 +358,31 @@ Optional argument ARG word to complete."
   (obsidian-update-tags-list)
   (obsidian--update-all-from-front-matter))
 
-(defun obsidian--request-link ()
-  "Service function to request user for link iput."
+(defun obsidian--format-link (file-path toggle)
+  "Format link based on `obsidian-use-vault-path' and an optional prefix argument"
+  (if obsidian-links-use-vault-path
+      (if toggle (file-name-nondirectory file-path) file-path)
+    (if toggle file-path (file-name-nondirectory file-path))))
+
+(defun obsidian--request-link (&optional toggle-path)
+  "Service function to request user for link input.
+
+TOGGLE-PATH is a boolean that will toggle the behavior of
+`obsidian-use-vault-path' for this single link insertion."
   (let* ((all-files (->> (obsidian-list-all-files) (-map (lambda (f) (file-relative-name f obsidian-directory)))))
          (region (when (use-region-p)
                    (buffer-substring-no-properties (region-beginning) (region-end))))
          (chosen-file (completing-read "Link: " all-files))
          (default-description (-> chosen-file file-name-nondirectory file-name-sans-extension))
-         (description (read-from-minibuffer "Description (optional): " (or region default-description))))
-    (list :file chosen-file :description description)))
+         (description (read-from-minibuffer "Description (optional): " (or region default-description)))
+         (file-link (obsidian--format-link chosen-file toggle-path)))
+    (list :file file-link :description description)))
 
 ;;;###autoload
-(defun obsidian-insert-wikilink ()
+(defun obsidian-insert-wikilink (&optional arg)
   "Insert a link to file in wikiling format."
-  (interactive)
-  (let* ((file (obsidian--request-link))
+  (interactive "P")
+  (let* ((file (obsidian--request-link arg))
          (filename (plist-get file :file))
          (description (plist-get file :description))
          (no-ext (file-name-sans-extension filename))
@@ -378,10 +392,10 @@ Optional argument ARG word to complete."
     (insert link)))
 
 ;;;###autoload
-(defun obsidian-insert-link ()
+(defun obsidian-insert-link (&optional arg)
   "Insert a link to file in markdown format."
-  (interactive)
-  (let* ((file (obsidian--request-link)))
+  (interactive "P")
+  (let* ((file (obsidian--request-link arg)))
     (-> (s-concat "[" (plist-get file :description) "](" (->> (plist-get file :file) (s-replace " " "%20")) ")")
         insert)))
 
