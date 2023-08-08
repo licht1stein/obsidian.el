6ad2861fea6b88e490d5b0a306a0921614a0b425
Mykhaylo Bilyanskyy <35418634+licht1stein@users.noreply.github.com>
2023-08-08 23:43:42 +0200

    Bump version

--- obsidian.el
+++ obsidian.el
@@ -5,7 +5,7 @@
 ;; Author: Mykhaylo Bilyanskyy
 ;; URL: https://github.com./licht1stein/obsidian.el
 ;; Keywords: obsidian, pkm, convenience
-;; Version: 1.3.2
+;; Version: 1.3.3
 ;; Package-Requires: ((emacs "27.2") (s "1.12.0") (dash "2.13") (markdown-mode "2.5") (elgrep "1.0.0") (yaml "0.5.1"))
 
 ;; This file is NOT part of GNU Emacs.
