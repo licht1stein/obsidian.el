4bc15833e2033b2b4b84f2ef444d4adefdbe4759
Joe Reinhart <joseph.reinhart@gmail.com>
2024-07-10 16:20:59 -0700

    Remove period from project URL

--- obsidian.el
+++ obsidian.el
@@ -3,7 +3,7 @@
 ;; Copyright (c) 2022 Mykhaylo Bilyanskyy <mb@blaster.ai>
 
 ;; Author: Mykhaylo Bilyanskyy
-;; URL: https://github.com./licht1stein/obsidian.el
+;; URL: https://github.com/licht1stein/obsidian.el
 ;; Keywords: obsidian, pkm, convenience
 ;; Version: 1.4.4
 ;; Package-Requires: ((emacs "27.2") (f "0.2.0") (s "1.12.0") (dash "2.13") (markdown-mode "2.5") (elgrep "1.0.0") (yaml "0.5.1"))
